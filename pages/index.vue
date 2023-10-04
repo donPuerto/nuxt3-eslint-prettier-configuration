b7a4a3cf4204858411b425c7469d75be763ab652
donPuerto <don.puerto@hotmail.com>
2023-10-04 17:39:08 +0800

    ..

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,15 @@
-<template>
-  <div />
-</template>
-
-<script lang="ts" setup>
+<script lang="ts">
 
 </script>
+
+<template>
+  <MyComponent lorem="1" />
+  <MyComponent lorem="1" ipsum="2" />
+  <MyComponent
+    lorem="1"
+    ipsum="2"
+  />
+</template>
 
 <style>
 </style>
