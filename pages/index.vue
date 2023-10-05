3a98eb2d0be2c27b0eb6ac18af5b0b0e6463b312
donPuerto <don.puerto@hotmail.com>
2023-10-05 17:15:44 +0800

    I think this is a good setup for eslint and prettier

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,15 @@
-<script lang="ts">
-
-</script>
+<script lang="ts"></script>
 
 <template>
   <MyComponent lorem="1" />
-  <MyComponent lorem="1" ipsum="2" />
+  <MyComponent
+    lorem="1"
+    ipsum="2"
+  />
   <MyComponent
     lorem="1"
     ipsum="2"
   />
 </template>
 
-<style>
-</style>
+<style></style>
